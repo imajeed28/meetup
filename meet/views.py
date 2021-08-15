990940a624a6357d0d8f734833d9c1075bad8769
Abdul Majeed <inamdarmajeed94@gmail.com>
2021-08-16 01:47:00 +0530

    Added comment and delete function

--- meet/views.py
+++ meet/views.py
@@ -1,12 +1,14 @@
+from io import SEEK_CUR
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views import View
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm 
 from django.views.generic.edit import UpdateView, DeleteView
 
 # Create your views here.
-class PostListView(View):
+class PostListView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_on')
         form = PostForm()
@@ -32,19 +34,42 @@ class PostListView(View):
         }
         return render(request, 'meet/post_list.html', context)
 
-class PostDetailView(View):
+class PostDetailView(LoginRequiredMixin, View):
     def get(self, request, pk, *args, **kwargs):
         post = Post.objects.get(pk=pk)
         form = CommentForm()
 
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+
         context = {
             'post': post,
             'form': form,
+            'comments': comments,
         }
 
         return render(request, 'meet/post_detail.html', context)
 
-class PostEditView(UpdateView):
+    def post(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+
+        context = {
+            'post': post,
+            'form': form,
+            'comments': comments,
+        }
+
+        return render(request, 'meet/post_detail.html', context)
+
+class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['body']
     template_name = 'meet/post_edit.html'
@@ -53,7 +78,27 @@ class PostEditView(UpdateView):
         pk = self.kwargs['pk']
         return reverse_lazy('post_detail', kwargs={'pk': pk})
 
-class PostDeleteView(DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'meet/post_delete.html'
     success_url = reverse_lazy('post_list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'meet/comment_delete.html'
+    
+    def get_success_url(self):
+        pk = self.kwargs['post_pk']
+        return reverse_lazy('post_detail', kwargs={'pk': pk})
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
