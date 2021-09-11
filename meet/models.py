5bd326f392287a0d38d7fd453b6d9020a4fbf36a
Abdul Majeed <inamdarmajeed94@gmail.com>
2021-09-11 09:30:04 +0530

    user profile created
    user profile created for each user and also added profile updation feature

--- meet/models.py
+++ meet/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver 
 
 # Create your models here.
 class Post(models.Model):
@@ -15,3 +18,20 @@ class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile', on_delete=models.CASCADE)
+	name = models.CharField(max_length=30, blank=True, null=True)
+	bio = models.TextField(max_length=500, blank=True, null=True)
+	interest = models.CharField(max_length=100, blank=True, null=True)
+	birth_date =models.DateField(null=True, blank=True)
+	location = models.CharField(max_length=100, blank=True, null=True)
+	picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.png', blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
--- meet/urls.py
+++ meet/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('post/edit/<int:pk>/', PostEditView.as_view(), name='post_edit'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
     path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment_delete'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
 ]
